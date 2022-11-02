04dc61d13e4ff1cc63a80c70e257fa66fd1bdb9e
Zin Moe <dev.zinmoe@gmail.com>
2022-11-02 23:08:24 +0630

    update deploy user and ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{ec2-user@ip-172-31-39-154}
-role :web, %w{ec2-user@ip-172-31-39-154}
-role :db,  %w{ec2-user@ip-172-31-39-154}
+role :app, %w{ubuntu@ip-172-31-46-26}
+role :web, %w{ubuntu@ip-172-31-46-26}
+role :db,  %w{ubuntu@ip-172-31-46-26}
 set :ssh_options, {
    keys: %w(/Users/zin/.ssh/ecs-rails-key.pem),
    forward_agent: false,
